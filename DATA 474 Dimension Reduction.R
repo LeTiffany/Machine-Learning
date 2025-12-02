6712301e76b86b38c4673953e72c64e9ce701fcf
Tiffany Le <tiffany.mc.le@gmail.com>
2025-12-02 15:58:08 -0700

    Do PLS

--- DATA 474 Dimension Reduction.R
+++ DATA 474 Dimension Reduction.R
@@ -9,10 +9,14 @@ red_wine["type"] = 1
 white_wine <- read.csv("winequality-white.csv", header = TRUE, sep = ";")
 white_wine["type"] = 0
 wine <- bind_rows(red_wine, white_wine)
-wine$good <- ifelse(wine$quality > 5, 1, 0)
+wine$good <- ifelse(wine$quality > 6, 1, 0)
+
+hist(wine$quality, main = "Histogram of Wine Quality", xlab = "Wine Quality")
 
 train <- sample(6497, 5198)
 test <- wine[-train,]
+
+sum(wine$good) / nrow(wine)
 
 log_mod <- glm(good ~ volatile.acidity + residual.sugar + free.sulfur.dioxide + total.sulfur.dioxide + sulphates + alcohol, data = wine, family = binomial, subset = train)
 vif(log_mod)
@@ -25,6 +29,8 @@ mean(glm.pred != test$good)
 
 pls.fit <- plsr(good ~ volatile.acidity + residual.sugar + free.sulfur.dioxide + total.sulfur.dioxide + sulphates + alcohol, data = wine , subset = train , scale = TRUE, validation = "CV")
 summary(pls.fit)
+
+plot(RMSEP(pls.fit), legendpos = "topright", xlab = "Components", main = "CV Error by Number of PLS Components")
 
 scores <- scores(pls.fit)
 
@@ -40,3 +46,13 @@ pls.pred <- rep(0, 1299)
 pls.pred[pls.probs > 0.5] <- 1
 
 mean(pls.pred != test$good)
+
+logit_pls_2 <- glm(good ~ c1 + c2, family = binomial, data = scores_train)
+
+test_scores_2 <- predict(pls.fit, newdata = test, type = "scores")
+
+pls.probs_2 <- predict(logit_pls_2, scores_test[, -3], type = "response")
+pls.pred_2 <- rep(0, 1299)
+pls.pred_2[pls.probs_2 > 0.5] <- 1
+
+mean(pls.pred_2 != test$good)
